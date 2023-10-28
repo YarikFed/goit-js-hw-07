01f876750fa85ec316d04180fda20dc7b75b8591
YarikFed <yarik.fyodorov.2001@gmail.com>
2023-10-28 19:55:34 +0200

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,7 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const galleryContainer = document.querySelector('.gallery');
+
 function renderGalleryItem(item) {
   const galleryItem = document.createElement('li');
   galleryItem.classList.add('gallery__item');
@@ -19,4 +20,17 @@ galleryItems.forEach((item) => {
   const galleryItem = renderGalleryItem(item);
   galleryContainer.appendChild(galleryItem);
 });
+
+galleryContainer.addEventListener('click', onGalleryItemClick);
+
+function onGalleryItemClick(event) {
+  event.preventDefault();
+
+  if (event.target.classList.contains('gallery__image')) {
+    const largeImageUrl = event.target.parentElement.href;
+
+    console.log('URL великого зображення:', largeImageUrl);
+  }
+}
+
 console.log(galleryItems);
