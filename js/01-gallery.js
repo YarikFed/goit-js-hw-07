3d5ec50a9fc9914dc29d85da17bc3218255ecc11
YarikFed <yarik.fyodorov.2001@gmail.com>
2023-10-28 19:50:01 +0200

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+function renderGalleryItem(item) {
+  const galleryItem = document.createElement('li');
+  galleryItem.classList.add('gallery__item');
 
+  const link = document.createElement('a');
+  link.classList.add('gallery__link');
+  link.href = item.original;
+  link.setAttribute('data-lightbox', 'gallery');
+  link.innerHTML = `<img class="gallery__image" src="${item.preview}" alt="${item.description}" />`;
+
+  galleryItem.appendChild(link);
+  return galleryItem;
+}
+
+galleryItems.forEach((item) => {
+  const galleryItem = renderGalleryItem(item);
+  galleryContainer.appendChild(galleryItem);
+});
 console.log(galleryItems);
