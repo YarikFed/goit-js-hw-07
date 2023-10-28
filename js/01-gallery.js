e505e9baec1bd10d358f97aeb07b09a5f2cbca67
YarikFed <yarik.fyodorov.2001@gmail.com>
2023-10-28 20:49:12 +0200

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,37 +1,21 @@
 import { galleryItems } from './gallery-items.js';
-
 const gallery = document.querySelector('.gallery');
 
-function renderGallery() {
- galleryItems.forEach((item) => {
-    const galleryItem = document.createElement('div');
-    galleryItem.classList.add('gallery-item');
+galleryItems.forEach((item) => {
+  const galleryItem = document.createElement('li');
+  galleryItem.classList.add('gallery__item');
 
-    const preview = document.createElement('img');
-    preview.classList.add('gallery-item-preview');
-    preview.src = item.preview;
-    preview.alt = item.description;
+  const galleryLink = document.createElement('a');
+  galleryLink.classList.add('gallery__link');
+  galleryLink.href = item.original;
 
-    const original = document.createElement('img');
-    original.classList.add('gallery-item-original');
-    original.src = item.original;
-    original.alt = item.description;
+  const galleryImage = document.createElement('img');
+  galleryImage.classList.add('gallery__image');
+  galleryImage.src = item.preview;
+  galleryImage.alt = item.description;
 
-    const description = document.createElement('div');
-    description.classList.add('gallery-item-description');
+  galleryLink.appendChild(galleryImage);
+  galleryItem.appendChild(galleryLink);
+  gallery.appendChild(galleryItem);
+});
 
-    const paragraph = document.createElement('p');
-    paragraph.textContent = item.description;
-
-    description.appendChild(paragraph);
-    galleryItem.appendChild(preview);
-    galleryItem.appendChild(original);
-    galleryItem.appendChild(description);
-
-    gallery.appendChild(galleryItem);
- });
-}
-
-renderGallery();
-
-console.log(galleryItems);
