f1276a6862c4af4fe69694885cd4b3e1331c7c54
YarikFed <yarik.fyodorov.2001@gmail.com>
2023-10-30 14:48:54 +0100

    1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
 console.log(galleryItems);
+
+const divRef = document.querySelector('.gallery');
+
+function createGallaryMarkup(items) {
+  return items
+    .map(
+      (item) => `<div class="gallery__item">
+                  <a class="gallery__link" href="${item.original}">
+                    <img
+                      class="gallery__image"
+                      src="${item.preview}" 
+                      data-source="${item.original}" 
+                      alt="${item.description}"
+                    />
+                  </a>
+                </div>`
+    )
+    .join("");
+}
+
+const addGallaryMarkup = createGallaryMarkup(galleryItems);
+
+divRef.innerHTML = addGallaryMarkup;
+
+divRef.addEventListener("click", onImageClick);
+
+function onImageClick(evt) {
+  blockStandartAction(evt);
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+  divRef.addEventListener("keydown", (evt) => {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  });
+}
+
+function blockStandartAction(evt) {
+  evt.preventDefault();
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionDelay: 250,
+});
