7b4a005061b7515e0af5af6883a03d3a49d88e0a
YarikFed <yarik.fyodorov.2001@gmail.com>
2023-10-30 14:33:59 +0100

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,25 +1,50 @@
 import { galleryItems } from './gallery-items.js';
+console.log(galleryItems);
 
-const gallery = document.querySelector('.gallery');
+const divRef = document.querySelector('.gallery');
 
-galleryItems.forEach((item, index) => {
-  const galleryItem = document.createElement('li');
-  galleryItem.classList.add('gallery__item');
+function createGallaryMarkup(items) {
+  return items
+    .map(
+      (item) => `<div class="gallery__item">
+                  <a class="gallery__link" href="${item.original}">
+                    <img
+                      class="gallery__image"
+                      src="${item.preview}" 
+                      data-source="${item.original}" 
+                      alt="${item.description}"
+                    />
+                  </a>
+                </div>`
+    )
+    .join("");
+}
 
-  const galleryLink = document.createElement('a');
-  galleryLink.classList.add('gallery__link');
-  galleryLink.href = item.original;
+const addGallaryMarkup = createGallaryMarkup(galleryItems);
 
-  const galleryImage = document.createElement('img');
-  galleryImage.classList.add('gallery__image');
-  galleryImage.src = item.preview;
-  galleryImage.alt = item.description;
+divRef.innerHTML = addGallaryMarkup;
 
-  galleryLink.appendChild(galleryImage);
-  galleryItem.appendChild(galleryLink);
-  gallery.appendChild(galleryItem);
-});
+divRef.addEventListener("click", onImageClick);
 
+function onImageClick(evt) {
+  blockStandartAction(evt);
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+   const instance = basicLightbox.create(`
+      <img src="${evt.target.dataset.source}" width="800" height="600">
+    `);
+  instance.show();
+  divRef.addEventListener("keydown", (evt) => {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  });
+}
+
+function blockStandartAction(evt) {
+  evt.preventDefault();
+}
 // gallery.addEventListener('click', (event) => {
 //   event.preventDefault();
 //   const target = event.target;
@@ -33,3 +58,8 @@ galleryItems.forEach((item, index) => {
 // function openImageModal(imageUrl) {
 //   window.open(imageUrl, '_blank');
 // };
+
+//  const instance = basicLightbox.create(`
+//       <img src="${largeImageUrl}" width="800" height="600">
+//     `);
+//     instance.show();
